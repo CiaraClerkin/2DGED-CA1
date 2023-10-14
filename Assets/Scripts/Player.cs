df80b45be52e8482a18aabac27b475138645f462
CiaraClerkin <145358793+CiaraClerkin@users.noreply.github.com>
2023-10-14 15:28:21 +0100

    Improved Player Movement
    *Changed the speed and jumping power and wall jumping power.
    *Also improved comments and moved code into places that made more sense.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,7 +9,7 @@ public class Player : MonoBehaviour
 {
     //bendux 2D Player Movement In Unity
     private float horizontal;
-    private float speed = 8f;
+    private float speed = 6f;
     private float jumpingPower = 7f;
     private bool isFacingRight = true;
 
@@ -22,10 +22,11 @@ public class Player : MonoBehaviour
     private float wallJumpingTime = 0.2f;
     private float wallJumpingCounter;
     private float wallJumpingDuration = 0.4f;
-    private UnityEngine.Vector2 wallJumpingPower = new UnityEngine.Vector2(4f, 8f);
+    private UnityEngine.Vector2 wallJumpingPower = new UnityEngine.Vector2(9f, 7f);
 
 
     [SerializeField] private Rigidbody2D rb;
+    
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
     
@@ -36,10 +37,11 @@ public class Player : MonoBehaviour
 
 
     void Update() {
-        //variable stores left and right movement
+        //horizontal stores -1 if moving left, 1 if moving right, 0 if no direction
         horizontal = Input.GetAxisRaw("Horizontal");
 
-        //jumping. the longer jump button is held down, the bigger the jump is. jumpingPower is the max, jumps can be shorter than it with short presses.
+        //jumping. the longer jump button is held down, the bigger the jump is. jumpingPower is the max, 
+        //jumps can be shorter than it with short presses.
         if (Input.GetButtonDown("Jump") && IsGrounded()) {
             rb.velocity = new UnityEngine.Vector2(rb.velocity.x, jumpingPower);
         }
@@ -50,78 +52,16 @@ public class Player : MonoBehaviour
         WallSlide();
         WallJump();
 
-        // flip is called when not wall jumping so wall jump can work.
+        //flip is called when not wall jumping so wall jump can work.
         if (!isWallJumping) {
             Flip();        
         }
     }
 
-    
-    //Capitalisation the start of methods is important  
-    
-    //checks if player is touching ground
-    private bool IsGrounded() {
-        //creates a 0.2f radius circle where groundCheck is and checks if groundLayer touches it.
-        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
-    }
-
-    //checks if player is touching a wall
-    private bool IsWalled() {
-        //same as isGrounded but for wall.
-        //wallCheck is on the right side of the player as player faces right by default.
-        return Physics2D.OverlapCircle(wallCheck.position, 0.2f, wallLayer);
-    }
-
-
-    //Not entirely sure how to explain WallSlide or WallJump, will come back to them later
-    private void WallSlide() {
-        if (IsWalled() && !IsGrounded() && horizontal != 0f) {
-            isWallSliding = true;
-            rb.velocity = new UnityEngine.Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -wallSlidingSpeed, float.MaxValue));
-        }
-        else {
-            isWallSliding = false;
-        }
-    }
-
-    private void WallJump() {
-        if (isWallSliding) {
-            isWallJumping = false;
-            wallJumpingDirection = -transform.localScale.x;
-            wallJumpingCounter = wallJumpingTime;
-        
-            CancelInvoke(nameof(StopWallJumping));
-        }
-        else {
-            wallJumpingCounter -= Time.deltaTime;
-        }
-
-        if (Input.GetButtonDown("Jump") && wallJumpingCounter > 0f) {
-            isWallJumping = true;   
-            rb.velocity = new UnityEngine.Vector2(wallJumpingDirection * wallJumpingPower.x, wallJumpingPower.y);
-            wallJumpingCounter = 0f;
-
-            if (transform.localScale.x != wallJumpingDirection) {
-                isFacingRight = !isFacingRight;
-                UnityEngine.Vector3 localScale = transform.localScale;
-                localScale.x *= -1f;
-                transform.localScale = localScale;
-            }
-
-            Invoke(nameof(StopWallJumping), wallJumpingDuration);     
-        }
-    }
-
-    private void StopWallJumping() {
-        isWallJumping = false;
-
-    }
-
-    //this is where player movement actually happens. 
-    //we don't let the player control movement during wall jumping, 
-    //the WallJump method pushes the player off the wall when they jump, so it has its own movement code.
     private void FixedUpdate() {
+        //player doesn't control horizontal movement during wall jumping, WallJump has its own movement code.
         if (!isWallJumping) {
+            //this is where horizontal player movement happens 
             rb.velocity = new UnityEngine.Vector2(horizontal * speed, rb.velocity.y);
         }
     }
@@ -136,4 +76,72 @@ public class Player : MonoBehaviour
         }
     }
 
+
+    
+    //Capitalisation the start of methods is important  
+    
+
+    private bool IsGrounded() {
+        //creates a 0.2f radius circle where groundCheck is and checks if groundLayer touches it.
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    private bool IsWalled() {
+        //same as isGrounded but for wall.
+        //wallCheck is on the right side of the player as player faces right by default.
+        return Physics2D.OverlapCircle(wallCheck.position, 0.2f, wallLayer);
+    }
+
+
+    private void WallSlide() {
+        // if the player moves into a wall while in the air,
+        // they wall slide and can wall jump
+        if (IsWalled() && !IsGrounded() && horizontal != 0f) {
+            isWallSliding = true;
+            //
+            rb.velocity = new UnityEngine.Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -wallSlidingSpeed, float.MaxValue));
+        }
+        else {
+            isWallSliding = false;
+        }
+    }
+
+
+    private void WallJump() {
+        if (isWallSliding) {
+            isWallJumping = false;
+            //stores the direction the player should face
+            wallJumpingDirection = -transform.localScale.x;
+            wallJumpingCounter = wallJumpingTime;
+        
+            CancelInvoke(nameof(StopWallJumping));
+        }
+        else {
+            //time limit on wall jump. ensures player can't spam and constantly wall jump with that
+            wallJumpingCounter -= Time.deltaTime;
+        }
+
+        
+        if (Input.GetButtonDown("Jump") && wallJumpingCounter > 0f) {
+            isWallJumping = true;
+            //player wall jump changes both x and y   
+            rb.velocity = new UnityEngine.Vector2(wallJumpingDirection * wallJumpingPower.x, wallJumpingPower.y);
+            wallJumpingCounter = 0f;
+            
+            //ensures that player is facing correct direction for the wall jump
+            if (transform.localScale.x != wallJumpingDirection) {
+                isFacingRight = !isFacingRight;
+                UnityEngine.Vector3 localScale = transform.localScale;
+                localScale.x *= -1f;
+                transform.localScale = localScale;
+            }
+
+            //Waits for the length of WallJumpingDuration and then calls StopWallJumping
+            Invoke(nameof(StopWallJumping), wallJumpingDuration);     
+        }
+    }
+
+    private void StopWallJumping() {
+        isWallJumping = false;
+    }
 }
